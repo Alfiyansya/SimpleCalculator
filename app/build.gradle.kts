01310d6b9128173014b38e906588cca8c30833e9
achmadalfiansyah <achmadalfiansyah069@gmail.com>
2024-05-21 22:41:04 +0700

    Building a UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Compose Dependency
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
 }
---
+++ app/src/main/java/com/alfiansyah/simplecalculator/CalculatorState.kt
@@ -0,0 +1,7 @@
+package com.alfiansyah.simplecalculator
+
+data class CalculatorState(
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null,
+)
