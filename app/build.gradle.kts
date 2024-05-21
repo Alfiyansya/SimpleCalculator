543a77bc2bd1ec952d31ca7104708b39cf2eb61a
achmadalfiansyah <achmadalfiansyah069@gmail.com>
2024-05-21 23:59:14 +0700

    Finish the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.alfiansyah.simplecalculator"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.alfiansyah.simplecalculator"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
