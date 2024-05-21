2fea99888281d793752ab468521b9e16cba787bf
achmadalfiansyah <achmadalfiansyah069@gmail.com>
2024-05-22 00:02:55 +0700

    updated the code

--- app/src/main/java/com/alfiansyah/simplecalculator/CalculatorViewModel.kt
+++ app/src/main/java/com/alfiansyah/simplecalculator/CalculatorViewModel.kt
@@ -5,32 +5,43 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
-class CalculatorViewModel: ViewModel() {
-
+class CalculatorViewModel : ViewModel() {
     var state by mutableStateOf(CalculatorState())
+        private set
 
     fun onAction(action: CalculatorAction) {
-        when(action) {
+        when (action) {
             is CalculatorAction.Number -> enterNumber(action.number)
-            is CalculatorAction.Delete -> delete()
+            is CalculatorAction.Decimal -> enterDecimal()
             is CalculatorAction.Clear -> state = CalculatorState()
             is CalculatorAction.Operation -> enterOperation(action.operation)
-            is CalculatorAction.Decimal -> enterDecimal()
-            is CalculatorAction.Calculate -> calculate()
+            is CalculatorAction.Calculate -> performCalculation()
+            is CalculatorAction.Delete -> performDeletion()
         }
     }
 
-    private fun enterOperation(operation: CalculatorOperation) {
-        if(state.number1.isNotBlank()) {
-            state = state.copy(operation = operation)
+    private fun performDeletion() {
+        when {
+            state.number2.isNotBlank() -> state = state.copy(
+                number2 = state.number2.dropLast(1)
+            )
+
+            state.operation != null -> state = state.copy(
+                operation = null
+            )
+            state.number1.isNotBlank() -> state = state.copy(
+                number1 = state.number1.dropLast(1)
+            )
+
         }
+
     }
 
-    private fun calculate() {
+    private fun performCalculation() {
         val number1 = state.number1.toDoubleOrNull()
         val number2 = state.number2.toDoubleOrNull()
-        if(number1 != null && number2 != null) {
-            val result = when(state.operation) {
+        if(number1 != null && number2 != null){
+            val result = when(state.operation){
                 is CalculatorOperation.Add -> number1 + number2
                 is CalculatorOperation.Subtract -> number1 - number2
                 is CalculatorOperation.Multiply -> number1 * number2
@@ -43,38 +54,37 @@ class CalculatorViewModel: ViewModel() {
                 operation = null
             )
         }
+
+
     }
 
-    private fun delete() {
-        when {
-            state.number2.isNotBlank() -> state = state.copy(
-                number2 = state.number2.dropLast(1)
-            )
-            state.operation != null -> state = state.copy(
-                operation = null
-            )
-            state.number1.isNotBlank() -> state = state.copy(
-                number1 = state.number1.dropLast(1)
-            )
+    private fun enterOperation(operation: CalculatorOperation) {
+        if (state.number1.isNotBlank()) {
+            state = state.copy(operation = operation)
         }
+
+
     }
 
     private fun enterDecimal() {
-        if(state.operation == null && !state.number1.contains(".") && state.number1.isNotBlank()) {
+        if(state.operation == null && !state.number1.contains(".") && state.number1.isNotBlank()){
             state = state.copy(
                 number1 = state.number1 + "."
             )
             return
-        } else if(!state.number2.contains(".") && state.number2.isNotBlank()) {
-            state = state.copy(
-                number2 = state.number2 + "."
-            )
         }
+        if(!state.number2.contains(".") && state.number2.isNotBlank()){
+            state = state.copy(
+                number1 = state.number2 + "."
+            )
+
+        }
+
     }
 
     private fun enterNumber(number: Int) {
-        if(state.operation == null) {
-            if(state.number1.length >= MAX_NUM_LENGTH) {
+        if (state.operation == null){
+            if (state.number1.length >= MAX_NUM_LENGTH){
                 return
             }
             state = state.copy(
@@ -82,15 +92,14 @@ class CalculatorViewModel: ViewModel() {
             )
             return
         }
-        if(state.number2.length >= MAX_NUM_LENGTH) {
+        if (state.number2.length >= MAX_NUM_LENGTH){
             return
         }
         state = state.copy(
             number2 = state.number2 + number
         )
     }
-
-    companion object {
+    companion object{
         private const val MAX_NUM_LENGTH = 8
     }
 }
